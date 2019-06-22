75bb90d829fbfffed575b92c8fab52e7cf47023f
Chad Evans <chad.evans@salesforce.com>
2019-06-21 22:30:55 -0400

    Deployer error correction

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f configproject-scratch-def.json -d 30 -s -w 60
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n RxManager_Admin
